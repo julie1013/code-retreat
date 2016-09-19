6655c11ee507e1019f985a01b72353f6eb085dae
julie1013 <julie1380@verizon.net>
2016-09-19 13:50:24 -0400

    Commit for cell class
    Committed for cell class so next time Brian decides to be an ass we won't
    lose anything. ;)

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,16 +1,25 @@
 class Board
   def initialize_board(n, val)
-  board = []
-  n.times do |row_index|
-    row = []
-    n.times { |column_index| row << val } # or row.push(val)
-    board << row # or board.push(val)
+    board = []
+    n.times do |row_index|
+      row = []
+      n.times { |column_index| row << Cell.new } # or row.push(val)
+      board << row # or board.push(val)
+    end
+    board
   end
-  board
-end
+
+  class Cell
+    def initalize(row, column)
+      @status = false # cell starts off dead
+      @coordinates = [] # this is used to (ultimately, hopefully) find the cell's neighbors so we can check their status?
+    end
+  end
 end
 
 
 # so we should save / commit more often....
 
 #ASSHOLES!!!!!
+#class cell? type please inside board what you had
+#aligh?
